5c715e08c3da075c694d5b25257c9063ea061726
Roram <68767119+Roram-io@users.noreply.github.com>
2022-01-11 01:29:56 -0500

    Create Transaction.java

---
+++ src/main/java/com/nttdata/transactions/model/Transaction.java
@@ -0,0 +1,25 @@
+package com.nttdata.transactions.model;
+
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import lombok.ToString;
+import org.springframework.data.annotation.Id;
+import org.springframework.data.mongodb.core.mapping.Document;
+
+import java.util.Date;
+import java.util.UUID;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@ToString
+@Document(collection = "transaction")
+public class Transaction {
+    @Id
+    private String id = UUID.randomUUID().toString();
+    private String productId;
+    private String userId;
+    private double transactionAmount;
+    private Date transactionDate;
+}
